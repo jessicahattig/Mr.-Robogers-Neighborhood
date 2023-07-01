797b7677e8dadc0b31219eef821b0808f92d7d5f
jessicahattig <jessicahattig@gmail.com>
2023-07-01 16:08:00 -0700

    WIP: function show results.

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,21 @@
 window.addEventListener("load", function () {
   const form = document.querySelector("form");
   form.addEventListener("submit", beepBoop)
+  
+  
   let resetBtn = document.querySelector("button#reset");
-  document.getElementById("final-results").innertext = numberArray;
+  let numberArray = []; 
+  document.getElementById("final-result").innerText = numberArray;
+  let inputNumber = document.getElementById("number").value;
 
+resetBtn.addEventListener("click", function() {
+
+});
 
 //Business Logic
 function beepBoop(event) {
   event.preventDefault();
-  
-  let numberArray = [];
+  numberArray = [];
   for (let i = 0; i<= inputNumber; i++) {
     if (i.toString().includes("3")) {
       numberArray.push("Won't you be my neighbor?")
@@ -22,6 +28,6 @@ function beepBoop(event) {
       numberArray.push(i);
     }
   }
-  return numberArray;
+  document.getElementById("final-results").innerText = numberArray;
 }
 });
