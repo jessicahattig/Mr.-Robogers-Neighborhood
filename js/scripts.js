c8b8b75c0748094021ce7ced315482cb0ef69dd9
jessicahattig <jessicahattig@gmail.com>
2023-07-01 13:20:05 -0700

    BUGFIX: Function 1, test 2, pass.

--- js/scripts.js
+++ js/scripts.js
@@ -16,15 +16,31 @@
 //   return numberArray;
 // }
 
+// function beepBoop(inputNumber) {
+//   let numberArray = []
+//   for (let i = 0; i<= inputNumber; i++) {
+//     if (i.toString().includes("1")) {
+//       numberArray.push("Beep");
+//     } else if (i.toString().includes("2")) {
+//       numberArray.push("Boop")
+//     } else if (i.toString().includes("3")) {
+//       numberArray.push("Won't you be my neighbor?")
+//     } else {
+//       numberArray.push(i);
+//     }
+//   }
+//   return numberArray;
+// }
+
 function beepBoop(inputNumber) {
   let numberArray = [];
   for (let i = 0; i<= inputNumber; i++) {
-    if (i.toString().includes("1")) {
-      numberArray.push("Beep");
-    } else if (i.toString().includes("2")) {
+    if (i.toString().includes("2")) {
       numberArray.push("Boop")
+    } else if (i.toString().includes("1")) {
+      numberArray.push("Beep")
     } else {
-      numberArray.push(i);
+    numberArray.push(i);
     }
   }
   return numberArray;
