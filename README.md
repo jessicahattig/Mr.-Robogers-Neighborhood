877bc0b54ef67e4cf31ec4cd6ef36f2fd9b1b90d
jessicahattig <jessicahattig@gmail.com>
2023-07-01 12:27:05 -0700

    Function 1, test 2, pass.

--- README.md
+++ README.md
@@ -40,6 +40,10 @@ Test: "It should return an array of numbers from 0 to the user's inputted number
 Code: beepBoop(0);
 Expected Output: [0]
 
+Test: "It should replace numbers that contain a "1" with "Beep"
+Code: beepBoop(1)
+Expected Output: [0,"Beep"]
+
 ## License
 
 MIT
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
-//Business Function
+//Business Logic
 function beepBoop() {
-  let inputNumber = parseInt(prompt("Enter a Number:"));
+  let inputNumber = parseInt(prompt("Enter a Number:")); // will remote prompt after BI passes and UI is incorperated. 
   let numberArray = [];
   for (let i = 0; i <= inputNumber; i++) {
-    numberArray.push(i);
+    if (i === 1) {
+      numberArray.push("Beep")
+    } else {
+      numberArray.push(i);
+    }
   }
   return numberArray;
 }
