290a855735eef8cbd94b694a267ecec1cad6376f
jessicahattig <jessicahattig@gmail.com>
2023-07-01 13:46:15 -0700

    BUGFIX: Function 1, test 3, pass.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,9 @@
 function beepBoop(inputNumber) {
   let numberArray = [];
   for (let i = 0; i<= inputNumber; i++) {
-    if (i.toString().includes("1")) {
+    if (i.toString().includes("2")) {
+      numberArray.push("Boop")
+    } else if (i.toString().includes("1")) {
       numberArray.push("Beep");
     } else {
       numberArray.push(i);
