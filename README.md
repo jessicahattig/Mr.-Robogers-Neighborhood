93617a92f0c6fe01ebbe3e7fc5ac1b1fcd858cde
jessicahattig <jessicahattig@gmail.com>
2023-07-01 12:17:38 -0700

    Function 1, test 1, pass.

--- README.md
+++ README.md
@@ -34,6 +34,12 @@ _Will add_
 
 ## Tests
 
+Describe: beepBoop()
+
+Test: "It should return an array of numbers from 0 to the user's inputted number"
+Code: beepBoop(0);
+Expected Output: [0]
+
 ## License
 
 MIT
---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//Business Function
+function beepBoop() {
+  let inputNumber = parseInt(prompt("Enter a Number:"));
+  let numberArray = [];
+  for (let i = 0; i <= inputNumber; i++) {
+    numberArray.push(i);
+  }
+  return numberArray;
+}
