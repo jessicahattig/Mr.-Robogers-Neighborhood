08bbace3723caf2e38ad5e0071c689d08d2acbef
jessicahattig <jessicahattig@gmail.com>
2023-07-01 12:46:26 -0700

    BUGFIX: function 1, test 1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 //Business Logic
-function beepBoop() {
-  let inputNumber = parseInt(prompt("Enter a Number:")); // will remote prompt after BI passes and UI is incorperated. 
-  let numberArray = [];
-  for (let i = 0; i <= inputNumber; i++) {
-    if (i === 1) {
-      numberArray.push("Beep")
-    } else if (i === 2) {
-      numberArray.push("Boop")
-    } else if (i === 3) {
-      numberArray.push("Won't you be my neighbor?")
-    } else if {
-      numberArray.push(i); 
-    }
-  }
-  return numberArray;
-}
+// function beepBoop() {
+//   let inputNumber = parseInt(prompt("Enter a Number:")); // will remove prompt after BI passes and UI is incorperated. 
+//   let numberArray = [];
+//   for (let i = 0; i <= inputNumber; i++) {
+//     if (i === 1) {
+//       numberArray.push("Beep")
+//     } else if (i === 2) {
+//       numberArray.push("Boop")
+//     } else if (i === 3) {
+//       numberArray.push("Won't you be my neighbor?")
+//     } else {
+//       numberArray.push(i); 
+//     }
+//   }
+//   return numberArray;
+// }
